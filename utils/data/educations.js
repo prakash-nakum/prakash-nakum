e799865cf4f408d323cab88bd53292b5e517c765
prakash-nakum <130529222+prakash-nakum@users.noreply.github.com>
2025-09-23 12:17:28 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -15,6 +15,6 @@ export const educations = [
     id: 3,
     title: "Secondary School Certificate",
     duration: "2016 - 2017",
-    institution: "Shree Eklavya Shaikshnik Sankul",
+    institution: "Shree Eklavya Shaikshnik Sankuls",
   },
 ];
