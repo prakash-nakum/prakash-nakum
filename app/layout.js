39b7e5fded0e976e3a39a5075f4c14d54fd910ed
prakash-nakum <130529222+prakash-nakum@users.noreply.github.com>
2026-03-14 14:33:42 +0530

    Update layout.js
    Updated for SEO

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <meta name="google-site-verification" content="uiU-6qJAw6OXaaHtJlM3ctyDPCpNcH5AnbYodUIOWRA" />
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
@@ -27,6 +29,7 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
+     </head>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
